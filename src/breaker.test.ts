215b366f2464629da3514e7b1dd44917c31b8f90
Yuriy Bogomolov <cronus.cpp@gmail.com>
2019-05-07 13:01:50 +0300

    WIP: migration to fp-ts@2
    Use top-level `fold`
    Update to fp-ts v2
    Rewrite CB using fp-ts@next & pipeable interface
    Change CB to a Reader<E, State<S, T>>
    Add an export with singleton instance which uses mutable clojure-bound breaker state for more practical usage.
    Update to fp-ts@2.0.0-rc.2

--- src/breaker.test.ts
+++ src/breaker.test.ts
@@ -1,12 +1,18 @@
-import 'mocha';
-
 import { expect } from 'chai';
 import fetchMock from 'fetch-mock';
+import { either, fold } from 'fp-ts/lib/Either';
+import { constant } from 'fp-ts/lib/function';
+import { IORef } from 'fp-ts/lib/IORef';
+import { delay, task } from 'fp-ts/lib/Task';
+import 'mocha';
 
-import { delay } from 'fp-ts/lib/Task';
-import { circuitBreaker, defaultBreakerOptions } from './index';
+import { breakerClosed } from './helpers';
+import { circuitBreaker, defaultBreakerOptions, singletonBreaker } from './index';
+import { BreakerClosed } from './types';
 
 const ADDRESS = 'http://example.com';
+const yep = constant('yep');
+const nope = constant('nope');
 
 describe('Circuit Breaker', () => {
   after(() => {
@@ -14,21 +20,29 @@ describe('Circuit Breaker', () => {
   });
 
   it('should handle promise resolution', (done) => {
-    const fetcher = circuitBreaker<string>().run(defaultBreakerOptions);
-    const [, result] = fetcher(() => Promise.resolve('ok'));
-    result.run().then(
-      (res) => res.fold(
-        done,
+    const fetcher = circuitBreaker<string>()(defaultBreakerOptions);
+    const [result] = fetcher({
+      request: () => Promise.resolve('ok'),
+      breakerState: new IORef(new BreakerClosed(0)),
+    });
+    result().then(
+      (res) => either.bimap(
+        res,
+        (err) => done(err),
         (_) => done(),
       ),
     );
   });
 
   it('should handle promise rejection', (done) => {
-    const fetcher = circuitBreaker<string>().run(defaultBreakerOptions);
-    const [, result] = fetcher(() => Promise.reject('not ok'));
-    result.run().then(
-      (res) => res.fold(
+    const fetcher = circuitBreaker<string>()(defaultBreakerOptions);
+    const [result] = fetcher({
+      request: () => Promise.reject('not ok'),
+      breakerState: new IORef(new BreakerClosed(0)),
+    });
+    result().then(
+      (res) => either.bimap(
+        res,
         (_) => done(),
         (s) => done(`should not resolve: ${s}`),
       ),
@@ -37,10 +51,14 @@ describe('Circuit Breaker', () => {
 
   it('should handle promise rejection with Error', (done) => {
     const error = new Error('not ok');
-    const fetcher = circuitBreaker<string>().run(defaultBreakerOptions);
-    const [, result] = fetcher(() => Promise.reject(error));
-    result.run().then(
-      (res) => res.fold(
+    const fetcher = circuitBreaker<string>()(defaultBreakerOptions);
+    const [result] = fetcher({
+      request: () => Promise.reject(error),
+      breakerState: new IORef(new BreakerClosed(0)),
+    });
+    result().then(
+      (res) => either.bimap(
+        res,
         (l: Error) => {
           expect(l).to.equal(error);
           done();
@@ -56,18 +74,52 @@ describe('Circuit Breaker', () => {
       .mock(ADDRESS, Promise.reject('oops'), { repeat: 2 })
       .mock(ADDRESS, Promise.resolve('ok'), { overwriteRoutes: false });
     const options = { maxBreakerFailures: 1, resetTimeoutSecs: 1, breakerDescription: '' };
-    const fetcher = circuitBreaker<Response>().run(options);
+    const fetcher = circuitBreaker<Response>()(options);
 
-    const [ref, result1] = fetcher(() => fetch(ADDRESS));
-    await result1.run();
-    expect(ref.read.run().tag).to.equal('Closed');
-    const [, result2] = fetcher(() => fetch(ADDRESS), ref);
-    await result2.run();
-    expect(ref.read.run().tag).to.equal('Open');
-    await delay(1000, '').run();
-    const [, result] = fetcher(() => fetch(ADDRESS), ref);
-    const r3 = await result.run();
-    expect(r3.fold(() => 'nope', () => 'yep')).to.equal('yep');
-    expect(ref.read.run().tag).to.equal('Closed');
+    const [result1, { breakerState }] = fetcher({
+      request: () => fetch(ADDRESS),
+      breakerState: new IORef(breakerClosed(0)),
+    });
+    await result1();
+    expect(breakerState.read().tag).to.equal('Closed');
+    const [result2] = fetcher({ request: () => fetch(ADDRESS), breakerState });
+    await result2();
+    expect(breakerState.read().tag).to.equal('Open');
+    await delay(1000, task.of(''))();
+    const [result] = fetcher({ request: () => fetch(ADDRESS), breakerState });
+    const r3 = await result();
+
+    expect(fold(nope, yep)(r3)).to.equal('yep');
+    expect(breakerState.read().tag).to.equal('Closed');
+  });
+
+  it('should mutate internal singleton breaker state', async () => {
+    const fetch = fetchMock
+      .sandbox()
+      .mock(ADDRESS, Promise.reject('oops'), { repeat: 2 })
+      .mock(ADDRESS, Promise.resolve('ok'), { overwriteRoutes: false });
+    const options = { maxBreakerFailures: 1, resetTimeoutSecs: 2, breakerDescription: 'open' };
+    const promise = () => fetch(ADDRESS);
+
+    // This is a fetcher with MUTABLE internal state using a clojure:
+    const fetcher = singletonBreaker(options);
+
+    const resultTE1 = fetcher(promise);
+    const resultE1 = await resultTE1();
+    expect(fold(nope, yep)(resultE1)).to.equal('nope');
+
+    const resultTE2 = fetcher(promise);
+    const resultE2 = await resultTE2();
+    expect(fold(nope, yep)(resultE2)).to.equal('nope');
+
+    await delay(1000, task.of(''))();
+    const resultTE3 = fetcher(promise);
+    const resultE3 = await resultTE3();
+    expect(fold<Error, Response, string>((e) => e.message, yep)(resultE3)).to.equal('open');
+
+    await delay(1000, task.of(''))();
+    const resultTE4 = fetcher(promise);
+    const resultE4 = await resultTE4();
+    expect(fold<Error, Response, string>((e) => e.message, yep)(resultE4)).to.equal('yep');
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-// Adapted implementation of https://hackage.haskell.org/package/glue-core-0.4.2/docs/src/Glue-CircuitBreaker.html
-
-import { constFalse, constTrue, Lazy } from 'fp-ts/lib/function';
-import { io, IO } from 'fp-ts/lib/IO';
-import { IORef } from 'fp-ts/lib/IORef';
-import { Reader } from 'fp-ts/lib/Reader';
-import { fromIO, fromLeft, TaskEither, tryCatch } from 'fp-ts/lib/TaskEither';
+import { left } from 'fp-ts/lib/Either';
+import { constFalse, constTrue, Lazy, pipeOp as pipe } from 'fp-ts/lib/function';
+import * as IO from 'fp-ts/lib/IO';
+import { IORef, newIORef } from 'fp-ts/lib/IORef';
+import { State } from 'fp-ts/lib/State';
+import * as TE from 'fp-ts/lib/TaskEither';
 
 import { breakerClosed, breakerOpen, getCurrentTime } from './helpers';
-import { BreakerOptions, BreakerState, EnhancedFetch } from './types';
+import { BreakerEnvironment, BreakerOptions, BreakerState, CircuitBreaker } from './types';
 
 /**
  * Default circuit breaker options
@@ -18,13 +17,15 @@ export const defaultBreakerOptions: BreakerOptions = {
   breakerDescription: 'Circuit breaker is open',
 };
 
-export const circuitBreaker = <T>() => new Reader<BreakerOptions, EnhancedFetch<T>>(
-  (opts: BreakerOptions) => {
-    const failingCall = (): TaskEither<Error, T> => fromLeft(new Error(opts.breakerDescription));
+export const circuitBreaker = <T>(): CircuitBreaker<T> => (opts: BreakerOptions) => {
+  const failingCall = (): TE.TaskEither<Error, T> => TE.fromEither(left(new Error(opts.breakerDescription)));
 
-    const incErrors = (ref: IORef<BreakerState>): IO<void> => getCurrentTime().read.chain(
-      (currentTime) => ref.read.chain(
-        (state) => {
+  const incErrors = (ref: IORef<BreakerState>) =>
+    pipe(
+      getCurrentTime().read,
+      IO.chain((currentTime) => pipe(
+        ref.read,
+        IO.chain((state) => {
           switch (state.tag) {
             case 'Closed': {
               const errorCount = state.errorCount;
@@ -35,55 +36,80 @@ export const circuitBreaker = <T>() => new Reader<BreakerOptions, EnhancedFetch<
               }
             }
             case 'Open': {
-              return io.of<void>(undefined);
+              return IO.of<void>(undefined);
             }
           }
-        },
-      ),
+        }),
+      )),
     );
 
-    const callIfClosed = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TaskEither<Error, T> =>
-      tryCatch(request, (reason) =>
-        incErrors(ref).map(() => (reason instanceof Error) ? reason : new Error(String(reason))).run());
+  const callIfClosed = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TE.TaskEither<Error, T> =>
+    TE.tryCatch(
+      request,
+      (reason) => pipe(
+        incErrors(ref),
+        IO.map<void, Error>(() => (reason instanceof Error) ? reason : new Error(String(reason))),
+      )(),
+    );
 
-    const canaryCall = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TaskEither<Error, T> =>
-      callIfClosed(request, ref).chain((result: T) => fromIO(ref.write(breakerClosed(0)).chain(() => io.of(result))));
+  const canaryCall = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TE.TaskEither<Error, T> =>
+    pipe(
+      callIfClosed(request, ref),
+      TE.chain((result: T) => pipe(
+        ref.write(breakerClosed(0)),
+        IO.chain(() => IO.of(result)),
+        (a) => TE.taskEither.fromIO(a),
+      )),
+    );
 
-    const callIfOpen = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TaskEither<Error, T> =>
-      fromIO<Error, boolean>(getCurrentTime().read.chain(
-        (currentTime) => ref.read.chain(
-          (state) => {
-            switch (state.tag) {
-              case 'Closed':
-                return ref.write(state).map(constFalse);
-              case 'Open': {
-                if (currentTime > state.openEndTime) {
-                  return ref.write(breakerOpen(currentTime + (opts.resetTimeoutSecs * 1000))).map(constTrue);
-                }
-                return ref.write(state).map(constFalse);
-              }
-            }
-          },
-        ),
-      )).chain(
-        (canaryRequest) => canaryRequest ? canaryCall(request, ref) : failingCall(),
-      );
-
-    const breakerService = (
-      request: Lazy<Promise<T>>,
-      ref: IORef<BreakerState> = new IORef(breakerClosed(0)),
-    ): [IORef<BreakerState>, TaskEither<Error, T>] =>
-      [ref, fromIO<Error, BreakerState>(ref.read).chain(
-        (state: BreakerState) => {
+  const callIfOpen = (request: Lazy<Promise<T>>, ref: IORef<BreakerState>): TE.TaskEither<Error, T> =>
+    pipe(
+      getCurrentTime().read,
+      IO.chain((currentTime) => pipe(
+        ref.read,
+        IO.chain((state) => {
           switch (state.tag) {
             case 'Closed':
-              return callIfClosed(request, ref);
-            case 'Open':
-              return callIfOpen(request, ref);
+              return pipe(ref.write(state), IO.map(constFalse));
+            case 'Open': {
+              if (currentTime > state.openEndTime) {
+                return pipe(ref.write(breakerOpen(currentTime + (opts.resetTimeoutSecs * 1000))), IO.map(constTrue));
+              }
+              return pipe(ref.write(state), IO.map(constFalse));
+            }
           }
-        },
-      )];
+        })),
+      ),
+      (a) => TE.taskEither.fromIO<Error, boolean>(a),
+      TE.chain((canaryRequest) => canaryRequest ? canaryCall(request, ref) : failingCall()),
+    );
 
-    return breakerService;
-  },
-);
+  const breakerService: State<BreakerEnvironment<T>, TE.TaskEither<Error, T>> =
+    ({ breakerState, request }) => [
+      pipe(
+        breakerState.read,
+        (a) => TE.taskEither.fromIO<Error, BreakerState>(a),
+        TE.chain((state: BreakerState) => {
+          switch (state.tag) {
+            case 'Closed':
+              return callIfClosed(request, breakerState);
+            case 'Open':
+              return callIfOpen(request, breakerState);
+          }
+        }),
+      ),
+      { breakerState, request },
+    ];
+
+  return breakerService;
+};
+
+/**
+ * Gets a singleton circuit breaker with clojure-bound state.
+ * @param opts Breaker options
+ */
+export const singletonBreaker = (opts: BreakerOptions) => {
+  const breakerState = newIORef(breakerClosed(0))();
+  return <T>(request: Lazy<Promise<T>>): TE.TaskEither<Error, T> =>
+    circuitBreaker<T>()(opts)({ request, breakerState })[0];
+};
